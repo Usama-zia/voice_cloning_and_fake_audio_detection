163c91f65f03b183b1ca2c9737114036bfd4de5a
Usama Muhammadi <musamazia736@gmail.com>
2023-01-29 11:29:27 +0000

    Repo Initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='voice cloning and fake audio detection',
+    author='Usama Muhammadi',
+    license='MIT',
+)
